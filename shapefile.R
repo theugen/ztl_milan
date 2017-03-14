83e67e859d9cf80ddfce2de0a3bb96f8bbbc6e17
Eugenio Thieme <eugen@thieme.it>
2017-03-14 21:01:32 +0100

    First experiment with shapefiles in R.
    I can try to change colors...

---
+++ shapefile.R
@@ -0,0 +1,25 @@
+library(maptools)
+library(ggmap)
+library(rgdal)
+library(RColorBrewer)
+
+colors <- brewer.pal(9, "BuGn")
+
+#reading shapefile .shp
+utm <- readOGR(file.choose())
+#I need to project from utm to lon lat to see something on my map (the 
+#map I will get from google is in lon lat)
+#To see if shapefile is already projected, and which projection it has been used,
+# str(shapefile)
+utmtolat <- spTransform(utm, CRS('+proj=longlat +datum=WGS84'))
+#convert shapefile to dataframe
+utmtolat <- fortify(utmtolat)
+#Getting map from maps.google
+milan <-get_map(location = 'Milano', maptype = 'roadmap', zoom=15)
+milano_map <- ggmap(milan)
+milano_map
+#Adding a layer with polygons representing limited traffic zones 
+milano_map <- milano_map + geom_polygon(data=utmtolat, aes(x=long, y=lat, group=group),
+                                        color=colors[9], fill=colors[6])
+milano_map <- milano_map + labs(title='Limited traffic zones in Milan', x='Longitude', y='Latitude')
+milano_map
